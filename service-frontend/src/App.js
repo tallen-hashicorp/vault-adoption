55d91fd8616ddf308c0f3c84255e1b953627ced2
Tyler Allen <tyler.allen@hashicorp.com>
2023-05-10 14:54:55 +0100

    Name it correclty

--- service-frontend/src/App.js
+++ service-frontend/src/App.js
@@ -150,7 +150,7 @@ function App() {
 
         <header className="bg-white shadow">
           <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900">Services</h1>
           </div>
         </header>
         <main>
