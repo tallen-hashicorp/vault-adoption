5eff5538240fa01d974116ce9a45b998609a1c68
Tyler Allen <tyler.allen@hashicorp.com>
2023-05-11 16:02:34 +0100

    use 0.0.5

--- workspace-tenant-custom/main.tf
+++ workspace-tenant-custom/main.tf
@@ -1,11 +1,12 @@
 
 module "bootstrap" {
   source          = "app.terraform.io/vault-adoption/vault-demo-database/module"
-  version         = "0.0.3"
+  version         = "0.0.5"
   db_host         = "database-1.c9rxkaf43me4.us-east-1.rds.amazonaws.com"
   db_password     = "xNKBBVDP"
   name            = "tenant_1"
   namespace_path  = "admin/tenant_1"
+  group_id        = data.tfe_outputs.bootstrap.values.tenant_group_policy_id
 }
 
 terraform {
