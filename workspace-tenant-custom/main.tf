64115e3118d8b6f2ff5bf72afb13547de2de7819
Tyler Allen <tyler.allen@hashicorp.com>
2023-05-11 15:36:00 +0100

    use 0.0.3

--- workspace-tenant-custom/main.tf
+++ workspace-tenant-custom/main.tf
@@ -1,7 +1,7 @@
 
 module "bootstrap" {
   source          = "app.terraform.io/vault-adoption/vault-demo-database/module"
-  version         = "0.0.2"
+  version         = "0.0.3"
   db_host         = "database-1.c9rxkaf43me4.us-east-1.rds.amazonaws.com"
   db_password     = "xNKBBVDP"
   name            = "tenant_1"
