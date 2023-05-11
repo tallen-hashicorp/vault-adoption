24dac043c0ff105357d58c595ba29e3a276929e4
Tyler Allen <tyler.allen@hashicorp.com>
2023-05-11 16:42:42 +0100

    Updated

--- workspace-tenant-bootstrap/main.tf
+++ workspace-tenant-bootstrap/main.tf
@@ -1,6 +1,6 @@
 module "bootstrap" {
   source              = "app.terraform.io/vault-adoption/vault-demo-bootstrap/module"
-  version             = "0.0.1"
+  version             = "0.0.4"
   name                = "tenant_1"
   admin_user_names    = ["neo", "trinity", "morpheus"]
 }
