f0da450cbaef1f255572b15626cbc16b31cffae4
Tyler Allen <tyler.allen@hashicorp.com>
2023-05-11 15:20:42 +0100

    Updating to use HCP

--- workspace-tenant-custom/main.tf
+++ workspace-tenant-custom/main.tf
@@ -1,8 +1,18 @@
 module "bootstrap" {
-  source          = "../module-database"
+  source          = "app.terraform.io/vault-adoption/vault-demo-database/module"
+  version         = "0.0.2"
   db_host         = "database-1.c9rxkaf43me4.us-east-1.rds.amazonaws.com"
   db_password     = "xNKBBVDP"
   name            = "tenant_1"
   namespace_path  = "admin/tenant_1"
 }
 
+terraform {
+  cloud {
+    organization = "vault-adoption"
+
+    workspaces {
+      name = "tenant1-custom"
+    }
+  }
+}
